fd0331b39aba3621d2dd09edd6ae1b8f9e754491
Sander Randmets <Sander91112@gmail.com>
2020-09-21 11:29:59 +0300

    sort

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -9,11 +9,12 @@ class BookController extends Controller
 {
     public function index()
     {
+        $order = request('order') || 'asc' ? 'desc' : 'asc';
         $books = Book::query();
         if (request()->has('sort')) {
-            $books->orderBy(request()->query('sort'));
+            $books->orderBy(request()->query('sort'), $order);
         }
 
-        return view('welcome', ['books' => $books->paginate(20)]);
+        return view('welcome', ['books' => $books->paginate(20), 'order' => $order]);
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,63 +1,53 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet"><title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <link href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css" rel="stylesheet">
+    <title>Laravel</title>
 
-        <!-- Styles -->
-    </head>
-    <body class="p-12">
-       <table class="table-auto w-full">
-            <thead>
-                <tr>
-                    <td class="px-4 py-2">Pealkiri</td>
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-                    <td class="px-4 py-2"><a href="?sort=release_date">Aasta</a></td>
+    <!-- Styles -->
+</head>
 
-                    <td class="px-4 py-2"><a href="?sort=language">Keel</a></td>
+<body class="p-12">
+    <table class="table-auto w-full">
+        <thead>
+            <tr>
+                <td class="px-4 py-2"><a href="?sort=title&order={{ $order }}">Pealkiri</a></td>
+                <td class="px-4 py-2">Autor</td>
+                <td class="px-4 py-2"><a href="?sort=release_date&order={{ $order }}">Aasta</a></td>
+                <td class="px-4 py-2"><a href="?sort=language&order={{ $order }}">Keel</a></td>
+                <td class="px-4 py-2"><a href="?sort=price&order={{ $order }}">Hind</a></td>
+                <td class="px-4 py-2"><a href="?sort=stock_saldo&order={{ $order }}">Laoseis</a></td>
+                <td class="px-4 py-2"><a href="?sort=release_date&order={{ $order }}">Lehekülgi</a></td>
+                <td class="px-4 py-2"><a href="?sort=type&order={{ $order }}">Tüüp</a></td>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($books as $book)
+            <tr>
+                <td class="border px-4 py-2">{{ $book->title }}</td>
+                <td class="border px-4 py-2">{{ $book->release_date }}</td>
+                <td class="border px-4 py-2">{{ $book->language }}</td>
+                <td class="border px-4 py-2">{{ $book->price }}</td>
+                <td class="border px-4 py-2">{{ $book->stock_saldo }}</td>
+                <td class="border px-4 py-2">{{ $book->pages }}</td>
+                <td class="border px-4 py-2">{{ $book->type }}</td>
+            </tr>
 
-                    <td class="px-4 py-2"><a href="?sort=price">Hind</a></td>
+            @endforeach
 
-                    <td class="px-4 py-2"><a href="?sort=stock_saldo">Laoseis</a></td>
+        </tbody>
 
-                    <td class="px-4 py-2"><a href="?sort=release_date">Lehekülgi</a></td>
-
-                    <td class="px-4 py-2"><a href="?sort=release_date">Tüüp</a></td>
-                </tr>
-            </thead>
-            <tbody>
-
-                @foreach($books as $book)
-
-                <tr>
-                    <td class="border px-4 py-2">{{ $book->title }}</td>
-
-                    <td class="border px-4 py-2">{{ $book->release_date }}</td>
-
-                    <td class="border px-4 py-2">{{ $book->language }}</td>
-
-                    <td class="border px-4 py-2">{{ $book->price }}</td>
-
-                    <td class="border px-4 py-2">{{ $book->stock_saldo }}</td>
-
-                    <td class="border px-4 py-2">{{ $book->pages }}</td>
-
-                    <td class="border px-4 py-2">{{ $book->type }}</td>
-                </tr>
-
-                @endforeach
-
-            </tbody>
-
-       </table>
-<div>
-{{ $books->links() }}
-</div>
-    </body>
+    </table>
+    <div>
+        {{ $books->links() }}
+    </div>
+</body>
 
 </html>
