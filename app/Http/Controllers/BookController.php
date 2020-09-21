2697176a519eae9548361aa4af90ed4addfb7cbf
Sander Randmets <Sander91112@gmail.com>
2020-09-21 10:50:40 +0300

    layers

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,14 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-   public function index()
-   {
-       if (request()->has('sort')) {
-           $books = Book::orderBy(request()->query('sort'))->paginate(20);
-       }
-   }
+    public function index()
+    {
+        $books = Book::query();
+        if (request()->has('sort')) {
+            $books->orderBy(request()->query('sort'));
+        }
+
+        return view('welcome', ['books' => $books->paginate(20)]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,7 @@
 <?php
 
-use App\Book;
-
-use Illuminate\Support\Facades\DB;
-
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    //$books = App\Book::paginate(20);
-    //$books = Book::where('language', 'Swahili')->paginate(15);
-
-    if (request()->has('sort')) {
-        $books = Book::orderBy(request()->query('sort'))->paginate(20);
-    } else {
-        $books = Book::paginate(20);
-    }
-    //return view('welcome');
-    //return DB::table('books')->where('language', 'Swahili')->get();
-    //$books = Book::where('language', 'Swahili')->get();
-
-    return view('welcome', ['books' => $books]);
-});
+Route::get('/', [BookController::class, 'index']);
